2c58b16a95f266e7c486118fe925a680d132ef4d
Sethu Narayan Senthil <github@sethusenthil.com>
2022-12-22 17:36:27 -0500

    Major Updates
    - Fix Manifest V3 declarations
    - Enhance copy to clipboard button
    - Add Keyboard shortcut (Command+k)
    - Added snackbar to alert user on copy
    - Updated demo image for new version

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Copy for ChatGPT
-The purpose of this extention is to copy output given from ChatGPT with just a click of a button
-## This is still in development, I bede this quickly before I went to bed 💀
+The purpose of this extention is to copy output given from ChatGPT with just a click of a button or through a keyboard command (CMD+K on Mac or CTRL+K on Windows)
+## This is still in development, I made this quickly before I went to bed 💀
+
+## Known Bugs
+CMD+K shortcut does not work properly in some situations (because of outdated document references when page is navigated)
+Emoji is boxed out on some computers (since emoji font is not available, fix: to replace to image)
+Enhancement: Check convo length for changes before traversing through convo (removed in current version due to document reference inconsistency)
 
 # How to add extension
+Download Source
 Load unpacked extension
 Will publish on Chrome store once done
 
 ![Preview](media/demo.png)
+Plz don't tell my prof, thx!
--- init.js
+++ init.js
@@ -1,44 +1,78 @@
 (function () {
 
-  const chatContainer = document.querySelector('.flex .flex-col .items-center');
+  const showSnackbar = (message) => {
+    const snack = document.createElement('div');
+    snack.classList.add('snackbar');
+    snack.innerText = message;
+    document.body.appendChild(snack);
+    snack.classList.add('show');
+    setTimeout(function () {
+      snack.className = snack.className.replace("show", "");
+      snack.remove();
+    }, 3000);
+  };
 
-  let previousChatCount = 0;
+  const copyToClipboard = (str) => {
+    navigator.clipboard.writeText(str).then(function () {
+      console.log('Async: Copying to clipboard was successful!');
+    }, function (err) {
+      console.error('Async: Could not copy text: ', err);
+    });
+    showSnackbar('Copied to clipboard 📋');
+  };
+
+
+  document.addEventListener("keydown", function (event) {
+    const chatContainer = document.querySelector('.flex .flex-col .items-center');
+
+    if (event.key === "k" && event.ctrlKey) {
+      console.log('Copy Shortcut Pressed');
+      //when command k is pressed
+      const chatbubbles = chatContainer.querySelectorAll('main.w-full .border-b');
+      if (chatbubbles.length % 2 === 0) {
+        //if last chat is from bot
+        const lastChatBubble = chatbubbles.at(-1);
+        const text = lastChatBubble.querySelector('.markdown').innerText
+        copyToClipboard(text);
+      }
+
+    }
+  });
+
 
   const intervalId = window.setInterval(function () {
-    const chatbubbles = chatContainer.querySelectorAll('main.w-full .border-b');
+    const chatContainer = document.querySelector('.flex .flex-col .items-center');
 
-    if (previousChatCount != chatbubbles.length) {
-      previousChatCount = chatbubbles.length;
+    //console.log('probing for new chat bubbles');
+    const chatbubbles = chatContainer.querySelectorAll('main.w-full .border-b');
+    //console.log('chatbubbles', chatbubbles);
 
     chatbubbles.forEach((chatbox, i) => {
-        //console.log('chatbox', chatbox);
-        //first chat box needs to be from user, hence all the even chat bubbles are from bot
-        if ((i + 1) % 2 === 0) {
-          //it is a chat box from bot
-          let thumbContainer = chatbox.querySelector('.flex .self-end');
+      //console.log('chatbox', chatbox);
+      //first chat box needs to be from user, hence all the even chat bubbles are from bot
+      if ((i + 1) % 2 === 0) {
+        //it is a chat box from bot
+        let thumbContainer = chatbox.querySelector('.flex .self-end');
 
-          const CLIPBOARD_CLASS_NAME = 'copy-to-clipboard';
+        const CLIPBOARD_CLASS_NAME = 'copy-to-clipboard';
 
-          if (!thumbContainer.classList.contains(CLIPBOARD_CLASS_NAME)) {
-            thumbContainer.classList.add(CLIPBOARD_CLASS_NAME);
-            thumbContainer.innerHTML = 'Copy to Clipboard 📋';
+        if (!thumbContainer.classList.contains(CLIPBOARD_CLASS_NAME)) {
 
-            thumbContainer.addEventListener('click', function () {
+          thumbContainer.classList.add(CLIPBOARD_CLASS_NAME);
+          thumbContainer.innerHTML = 'Copy to Clipboard 📋';
 
-              const text = chatbox.querySelector('.markdown').innerText;
+          thumbContainer.addEventListener('click', function () {
 
-              navigator.clipboard.writeText(text).then(function() {
-                console.log('Async: Copying to clipboard was successful!');
-              }, function(err) {
-                console.error('Async: Could not copy text: ', err);
-              });
+            const text = chatbox.querySelector('.markdown').innerText;
+            copyToClipboard(text);
 
-            });
-          }
+          });
+        } else {
 
         }
+
+      }
     });
-  }
   }, 1000);
 
 
