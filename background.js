18e99de12ac56e37805f081d8f46d2a6a4bc2116
Sethu Narayan Senthil <github@sethusenthil.com>
2023-02-07 13:53:52 -0500

    More Features! v0.3.0
    - Run text through GPTZero to check if text is flagged and warn user
    
    - Added fancy console log status
    
    - Updated build script to included new background script perms

---
+++ background.js
@@ -0,0 +1,41 @@
+try {
+    // This is the background script for the extension
+
+    chrome.runtime.onMessage.addListener(receiver);
+
+    function receiver(request, sender, sendResponse) {
+        console.log("got a request", request.message)
+        let theBody = { "document": request.message }
+
+        //run text to plagiarism detectors
+        fetch("https://api.gptzero.me/v2/predict/text", {
+            "headers": {
+                "accept": "*/*",
+                "origin": "https://gptzero.me",
+                "accept-language": "en-US,en;q=0.9",
+                "content-type": "application/json",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-site",
+                "sec-gpc": "1",
+                "Referer": "https://gptzero.me/",
+                "Referrer-Policy": "strict-origin-when-cross-origin"
+            },
+            "body": JSON.stringify(theBody),
+            "method": "POST"
+        }).then(response => response.json()).then(data => {
+            console.log('data', data);
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id,
+                    {
+                        message: data
+                    }, function (response) { })
+            })
+        }).catch((error) => {
+            console.error('Request Error:', error);
+        });
+
+    }
+} catch (err) {
+    console.log(err);
+}
--- build.js
+++ build.js
@@ -16,6 +16,8 @@ const manifest = require('./manifest.json');
 //copy all files referenced in manifest.json to build folder
 const cs = manifest.content_scripts[0]
 
+cs.js.push(manifest.background.service_worker)
+
 cs.js.forEach((jsFile) => {
     fs.copyFileSync(jsFile, `build/${jsFile}`);
 });
